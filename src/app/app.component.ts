bcd2835a6e0da7581e310ec45b7bdab66d7b0fde
Kojoel <nanaabruquah1s@gmail.com>
2024-07-24 05:11:20 -0700

    Card complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./card/card.component";
+import { Card2Component } from "./card2/card2.component";
+import { Card3Component } from "./card3/card3.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, Card2Component, Card3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
